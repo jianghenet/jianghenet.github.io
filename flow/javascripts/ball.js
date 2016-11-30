bfb3bdbbfe24d8f2d31c872da952d43dc8fa54a2
姜河 <jianghe@autoeeby.cn>
2016-11-30 15:08:50 +0800

    移动到指定坐标

--- flow/javascripts/ball.js
+++ flow/javascripts/ball.js
@@ -5,9 +5,24 @@ function Ball(x, y, r){
 }
 
 Ball.prototype.down = function(){
-  this.y = this.y + 1;
+  this.y = this.y + this.speed;
 };
 
-Ball.prototype.renew = function(){
+Ball.prototype.renew = function(x, speed){
   this.y = 0;
+  if(speed){
+    this.speed = speed;
+  }
+  if(x){
+    this.x=x;
+  }
 };
+
+function Bullet(x, y, r){
+  this.x = x;
+  this.y = y;
+  this.r = r;
+  this.timeout = 0;
+}
+
+Bullet.prototype.constructor = Ball;
